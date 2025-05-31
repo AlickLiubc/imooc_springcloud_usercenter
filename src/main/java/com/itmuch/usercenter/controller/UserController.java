905158a7f3cdad31f16688e8016c0737f3e06fdd
AlickLiubc <nrlbc@sina.com>
2025-06-01 00:25:10 +0800

    补充日志记录相关代码

--- src/main/java/com/itmuch/usercenter/controller/UserController.java
+++ src/main/java/com/itmuch/usercenter/controller/UserController.java
@@ -3,6 +3,7 @@ package com.itmuch.usercenter.controller;
 import com.itmuch.usercenter.domain.entity.user.User;
 import com.itmuch.usercenter.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,12 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@Slf4j
 public class UserController {
 
     private final UserService userService;
 
     @GetMapping("/{id}")
     public User findById(@PathVariable("id") Integer id) {
+        log.info("我被请求了。。。");
         return userService.getUserById(id);
     }
 
